1856aaab1a8a13e08e65bc59712cb8f255045194
josepaulodelacruz <whoseepowlu@gmail.com>
2020-02-04 12:46:30 +0800

    added steps comments

--- charCount/index.js
+++ charCount/index.js
@@ -1,15 +1,19 @@
+// create a function that return a object letter accumulator
+// loop through all the character in the word
+//create an accumulator
+//check if its a repeated character
+
 function charCount (str) {
     let result = {}
-
     for(let i = 0 ; i < str.length ; i++) {
         let char = str[i]
         if(result[char] > 0) {
             result[char]++
-        }else {
+        } else {
             result[char] = 1
         }
-    }
+     }
     return result
 }
 
-console.log(charCount('Hello World'))
+console.log(charCount('aabbdefghiii'))
