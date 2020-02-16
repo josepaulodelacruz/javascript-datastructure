91c3adfcb3135512d9f65a12b0b9dc6210850639
josepaulodelacruz <whoseepowlu@gmail.com>
2020-02-16 11:13:50 +0800

    feat: frequency counter question

---
+++ frequency-counters/index.js
@@ -0,0 +1,15 @@
+/**
+ * Write a function called same, which accepts 2 arrays.
+ * The function should return true if every value in the array
+ * it's corresponding value squared in the second array.
+ * The frequency of valued must be the same
+ */
+
+function same(arr1, arr2) {
+    if(arr1.length !== arr2.length) {
+        return false
+    }
+    return ''
+}
+
+console.log(same())
