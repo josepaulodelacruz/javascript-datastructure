2902d971f8995c2c34b5f69bf140c8d6704d7671
josepaulodelacruz <whoseepowlu@gmail.com>
2020-02-20 16:30:51 +0800

    multiple pointers

--- anagram/index.js
+++ anagram/index.js
@@ -6,28 +6,27 @@
  */
 
 function validAnagram(string1, string2) {
-  if(string1.length !== string2.length) {
-    return false
-  }
-
-  let charCount = {}
-
-  for(let i = 0 ; i < string1.length ; i++) {
-    let letter = string1[i]
-    charCount[letter] ? charCount[letter] += 1 : charCount[letter] = 1
-  }
-
-  for(let i = 0 ; i < string2.length ; i++) {
-    let letter = string2[i]
-    if(!charCount[letter]) {
-      return false
-    } else {
-      charCount[letter] -= 1
+    if(string1.length !== string2.length) {
+        return false
     }
 
-  }
+    let charCount = {}
 
-  return true
+    for(let i = 0 ; i < string1.length ; i++) {
+        let letter = string1[i]
+        charCount[letter] ? charCount[letter] += 1 : charCount[letter] = 1
+    }
+
+    for(let i = 0 ; i < string2.length ; i++) {
+        let letter = string2[i]
+        if(!charCount[letter]){
+            return false
+        }else {
+            charCount[letter] -= 1
+        }
+    }
+
+    return true
 
 }
 
